dd285b36333c59eeaebcf93d35c848744af0e6a4
Joseph Wambua <101257432+josefwambua@users.noreply.github.com>
2024-02-08 12:05:10 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = () => {
             onClick={() => setActive(link.title)}
             
             >
-              <a href={`${link.id}`}>{link.title}</a>
+              <a href={`#${link.id}`}>{link.title}</a>
             </li>
           ))}
         </ul>
@@ -61,7 +61,7 @@ const Navbar = () => {
               setActive(link.title)}}
             
             >
-              <a href={`${link.id}`}>{link.title}</a>
+              <a href={`#${link.id}`}>{link.title}</a>
             </li>
           ))}
         </ul>
